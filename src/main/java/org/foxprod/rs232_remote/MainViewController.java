29f8ba2171bce8a9b1bf32d4edd5d4c10ddb4b77
Arkadii <arkady2003@gmail.com>
2025-07-04 02:27:07 +0400

    Implement loading properties, fill basic form layout

--- src/main/java/org/foxprod/rs232_remote/MainViewController.java
+++ src/main/java/org/foxprod/rs232_remote/MainViewController.java
@@ -1,10 +1,52 @@
 package org.foxprod.rs232_remote;
 
+import javafx.beans.property.Property;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import com.fazecast.jSerialComm.SerialPort;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Properties;
 
-public class MainViewController {
+import static java.util.Map.entry;
 
+public class MainViewController implements Initializable {
+
+    Properties properties = new Properties();
+
+    Map<String, String> codes = new LinkedHashMap<>();
+
+    @FXML
+    private Menu portMenu;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        SerialPort[] ports = SerialPort.getCommPorts();
+
+        for (SerialPort port : ports) {
+            portMenu.getItems().add(new MenuItem(port.getSystemPortPath().substring(4)));
+        }
+
+        loadCodes();
+    }
+
+    public void loadCodes() {
+        try(FileInputStream fileInputStream = new FileInputStream("config.properties")) {
+            properties.load(fileInputStream);
+            for (String key : properties.stringPropertyNames()) {
+                codes.put(key, properties.getProperty(key));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
