330247b04052bc23e96c1c3643daa7bfc4294c97
Arkadii <arkady2003@gmail.com>
2025-07-12 00:20:54 +0400

    Implement port opening on a button press, add button icons

--- src/main/java/org/foxprod/rs232_remote/MainViewController.java
+++ src/main/java/org/foxprod/rs232_remote/MainViewController.java
@@ -6,8 +6,6 @@ import com.fazecast.jSerialComm.SerialPort;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuItem;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -28,42 +26,26 @@ public class MainViewController implements Initializable {
     Boolean isPortOpened = false;
 
     @FXML
-    private Menu portMenu;
-
-    @FXML
     private Button openPortButton;
 
     @FXML
     private ComboBox<String> portsDropdown;
 
+    // Getting all ports from system and adding them to dropdown menu
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         SerialPort[] ports = SerialPort.getCommPorts();
 
         for (SerialPort port : ports) {
-            MenuItem item = new MenuItem(port.getSystemPortPath().substring(4));
-            item.setOnAction(event -> {
-                if (activePort != null) {
-                    try {
-                        portOutputStream.close();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    activePort.clearDTR();
-                    activePort.clearRTS();
-                    activePort.closePort();}
-                activePort = SerialPort.getCommPort(port.getSystemPortPath());
-                if (activePort.openPort()){
-                    System.out.println("Opened port" + activePort.getSystemPortPath());
-                }
-                portOutputStream = activePort.getOutputStream();
-            });
-            portMenu.getItems().add(item);
+            portsDropdown.getItems().add(port.getSystemPortPath().substring(4));
         }
+        portsDropdown.setValue(portsDropdown.getItems().getLast());
+
         loadCodes();
     }
 
+    // Loading codes from config inti HashMap
     public void loadCodes() {
         try(FileInputStream fileInputStream = new FileInputStream("config.properties")) {
             properties.load(fileInputStream);
@@ -75,6 +57,7 @@ public class MainViewController implements Initializable {
         }
     }
 
+    // Sending a command from String
     public void sendCommand(String command) {
         try {
             portOutputStream.write(new byte[] { (byte) Integer.parseInt(command, 16) });
@@ -85,54 +68,73 @@ public class MainViewController implements Initializable {
     }
 
     public void closePortOnExit() {
-        if (activePort != null) {
-            try {
-                portOutputStream.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            activePort.clearDTR();
-            activePort.clearRTS();
-            activePort.closePort();
-        }
+        closePort(activePort, portOutputStream);
     }
 
     public void onEjectPressed(ActionEvent actionEvent) {
+        if (!isPortOpened) {openPortButton.fire();}
         sendCommand(codes.get("eject"));
     }
 
     public void onRewPressed(ActionEvent actionEvent) {
+        if (!isPortOpened) {openPortButton.fire();}
         sendCommand(codes.get("rew"));
     }
 
     public void onPlayPressed(ActionEvent actionEvent) {
+        if (!isPortOpened) {openPortButton.fire();}
         sendCommand(codes.get("play"));
     }
 
     public void onFFPressed(ActionEvent actionEvent) {
+        if (!isPortOpened) {openPortButton.fire();}
         sendCommand(codes.get("ff"));
     }
 
     public void onStopPressed(ActionEvent actionEvent) {
+        if (!isPortOpened) {openPortButton.fire();}
         sendCommand(codes.get("stop"));
     }
 
+    // Opening port if not open yet and changing button text accordingly
     public void onOpenPortPressed(ActionEvent actionEvent) {
         if (isPortOpened){
+            closePort(activePort, portOutputStream);
+
+            isPortOpened = false;
+            openPortButton.setText("Open");
+        }
+        else {
+            activePort = SerialPort.getCommPort(portsDropdown.getValue());
+
+            portOutputStream = openPort(activePort);
+
+            isPortOpened = true;
             openPortButton.setText("Close");
+        }
+    }
+
+    public OutputStream openPort(SerialPort serialPort) {
+        if (serialPort != null) {
+        serialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 0, 0);
+
+        serialPort.openPort();
+
+        return serialPort.getOutputStream();
+        }
+        return null;
+    }
+
+    public void closePort(SerialPort serialPort, OutputStream outputStream) {
+        if (serialPort != null && serialPort.isOpen()) {
             try {
-                portOutputStream.close();
+                outputStream.close();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            activePort.closePort();
-        }
-        else {
-            try {
-                activePort = SerialPort.getCommPort(portsDropdown.getValue());
-
-                portOutputStream = activePort.getOutputStream();
-            }
+            serialPort.clearDTR();
+            serialPort.clearRTS();
+            serialPort.closePort();
         }
     }
 }
